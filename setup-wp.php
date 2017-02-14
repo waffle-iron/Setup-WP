61522874c5c205e5f13baccb8612ec10e93e6518
JayyWalker <jordan@jayy.online>
2017-02-14 16:15:07 +0000

    wIP

---
+++ setup-wp.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'vendor/autoload.php'; 
+
+
+
+/*
+ * TODO Checkout
+ * 
+ * Bitbucket API - https://gentlero.bitbucket.io/bitbucket-api/0.8/examples/
+ * git API - https://github.com/kbjr/Git.php
+ *
+ */
+$user = new Bitbucket\
