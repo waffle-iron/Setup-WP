383b19565093e518e3ce28ab1dfb01fbac8b57b0
Jordan Walker <jordan@jayy.online>
2017-02-15 02:21:37 +0000

    Plan

--- setup-wp.php
+++ setup-wp.php
@@ -1,8 +1,48 @@
 <?php
 
-require_once 'vendor/autoload.php'; 
+require 'vendor/autoload.php'; 
 
+/**
+ * TODO Process
+ *
+ * 1. Create an optional config file. If no config file, command arguments must be used.
+ * 2. Create repo if it does not exist.
+ * 3. Set origin.
+ * 4. Setup dev branch.
+ * 5. If config.setup is true, download latest Wordpress.
+ * 6. Download themes and plugins.
+ * 7. Create config.
+ * 8. Create mysql DB.
+ * 9. Create nginx vhost.
+ * 10. Fin.
+ */
 
+/*
+ * XXX Config options
+ *  account_name
+ *  repo_slug
+ *  wordpress:
+ *    setup: true/false
+ *    plugins: 
+ *      - 
+ *        - 
+ *          url: Specify only if not on Wordpress website. 
+ *          name: To rename the folder.
+ *          username: If HTTP request requires credentials.
+ *          password: If HTTP request requires credentials.
+ *    theme:
+ *      url: Check if bitbucket/github and use appropriate API HTTP request if not.
+ *      name: If changed, rename folder. If not use end of url.
+ *    config:
+ *      site_url
+ *      home_url
+ *      db_name
+ *      db_username
+ *      db_password
+ *      db_host
+ *      db_prefix
+ *    search_replace: URL to replace with config.site_url
+ */
 
 /*
  * TODO Checkout
@@ -11,4 +51,3 @@ require_once 'vendor/autoload.php';
  * git API - https://github.com/kbjr/Git.php
  *
  */
-$user = new Bitbucket\
